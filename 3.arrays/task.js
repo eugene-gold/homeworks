a0e49c51cd4e46d6028170944df5fa1dbfbd7f42
eugene_g <uran2@bk.ru>
2022-06-10 10:58:50 +0800

    task.js in arrays updated

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  // let result;
   // Ваш код
-
-  return result; // boolean
+  if(arr1.length === arr2.length && arr1.every((item,index) => item === arr2[index])) {
+    return true
+  }
+  else return false
+  // return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
-  return resultArr; // array
+  return resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10); // array
 }
