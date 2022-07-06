4203a0d4c7b64022c485a4d4109056f29570206c
eugene-gold <uran2@bk.ru>
2022-07-06 09:18:39 +0800

    a part of 8 task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,11 +1,52 @@
-function cachingDecoratorNew(func) {
+function cachingDecoratorNew (func) {
   // Ваш код
+    const cache = [];
+    function wrapper(...arguments) {
+        const hash = [arguments].join(',')
+        const objInCache = cache.find(item => item.hash === hash )
+        if (objInCache) {
+            console.log(`Из кэша: ${objInCache['value']}`)
+            return `Из кэша: ${objInCache['value']}`
+        }
+        const result = arguments.reduce((a,b) => a + b)
+        if(cache.length < 5) {
+            cache.push({['hash']: `${hash}`, ['value']: result})
+            console.log(`Вычисляем: ${result}`)
+            return `Вычисляем: ${result}`
+        } else {
+            cache.shift()
+            cache.push({['hash']: `${hash}`, ['value']: result})
+            console.log(`Вычисляем: ${result}`)
+            return `Вычисляем: ${result}`
+        }
+    }
+    return wrapper
 }
-
 
 function debounceDecoratorNew(func) {
   // Ваш код
+    const debounceDecorator = (f, ms) => {
+        let timerId;
+        let firstStart;
+        if (firstStart) {
+
+            return function (...args) {
+                setTimeout(() => {
+                    f.apply(this, args);
+                },);
+                firstStart = false;
+                clearTimeout(timerId);
+                timerId = setTimeout(() => {
+                    f.apply(this, args);
+                }, ms);
+
+            };
+        }
+    }
+
 }
+
+
 
 function debounceDecorator2(func) {
   // Ваш код
